189f23c87c9c10cd288af1c53097999fc2158062
Александр Кундрюков <a.kundryukov@gmail.com>
2025-05-11 18:02:08 +0300

    Implement new math calculator;

--- src/main/kotlin/me/pseudoapp/other/MathCalculator.kt
+++ src/main/kotlin/me/pseudoapp/other/MathCalculator.kt
@@ -10,14 +10,262 @@ fun main() {
     println((1 + ((((((1 * 2)) * (2 + 4))) * 3)) + (((12 + ((14 / 2))) * 2))))
     println("============= result: ")
 
-    println(calcMath(data))
-    println(calcMath("2*(-1)"))
+//    println(calcMath(data))
+    println(calcMath("(1+2*2+(1-2*2+1+(1-5*2+(-4+2*5))))"))
+//    println(calcMath("2*2*(1-2)"))
+//    println(calcMath("2*2*(1-2*(3+(48-51)))"))
+//    println(calcMath("1+1*2*(2+4)*3+(12+14/2)*2"))
+}//2*(-2-4)
+
+
+fun CharSequence.positionOf(
+    data: String,
+    startIndex: Int = 0,
+//    endIndex: Int = 0,
+    fromEndToStart: Boolean = false
+): Int {
+    if (fromEndToStart) {
+        val partSize = data.length
+        var partIndex = startIndex - partSize
+        while (partIndex > if (partSize == 1) 0 else 1) { // обрабатываем и если нечетное
+            val part = this.substring(partIndex, partIndex + partSize)
+//            println("part: $part")
+            if (part == data) {
+                return partIndex
+            }
+
+            partIndex -= partSize
+        }
+
+        return -1
+
+    } else {
+        return this.indexOf(data, startIndex)
+    }
+}
+
+fun CharSequence.positionOf(
+    check: (String) -> Boolean,
+    startIndex: Int = 0,
+    fromEndToStart: Boolean = false
+): Int {
+    val partSize = 1
+    if (fromEndToStart) {
+        var partIndex = startIndex - partSize
+        while (partIndex > if (partSize == 1) 0 else 1) {
+            val part = this.substring(partIndex, partIndex + partSize)
+//            println("part: $part")
+            if (check(part)) {
+                return partIndex
+            }
+
+            partIndex -= partSize
+        }
+
+        return -1
+
+    } else { // this, data
+        var partIndex = startIndex
+        while (partIndex + partSize <= this.length - if (partSize == 1) 0 else 1) {
+            val part = this.substring(partIndex, partIndex + partSize)
+//            println("part: $part")
+            if (check(part)) {
+                return partIndex
+            }
+
+            partIndex += partSize
+        }
+
+        return -1
+    }
+}
+
+fun calcMath(rowExpression: String): String {
+
+    val result = StringBuilder(
+        "(${rowExpression.replace(" ", "")})"
+    )
+    val digitsWithDot = "0123456789."
+
+    // 1. Сначала высчитываем то что в скобках
+    val ops = listOf('-', '+')
+    while (true) {
+        // идём вперед к первой закрывающей - это значит самая глубокая вложенность
+        val endIndex = result.positionOf(")")
+        if (endIndex == -1) {
+            break
+        }
+        // идём назад до ближайшей открывающей
+        val startIndex = result.positionOf(
+            "(",
+            startIndex = endIndex,
+            fromEndToStart = true
+        )
+        println("rowExpression: $rowExpression")
+        println("startIndex: $startIndex")
+        println("endIndex: $endIndex")
+
+        // 2. Вычисляем сложные операции (*, /, % и т.п.)
+
+        listOf('*', '/', '%').forEach { op ->
+            println("x100")
+            while (true) {
+                println("x200")
+                val endIndex0 = result.positionOf(")")
+                val opIndex = result.substring(0, endIndex0).positionOf(
+                    "$op",
+                    startIndex + 1
+                )
+                println("300")
+//                println("opIndex: ${opIndex}")
+                if (opIndex == -1) {
+                    break
+                }
+
+                var startIndex1 = result.positionOf(
+                    { !digitsWithDot.contains(it) },
+                    opIndex,
+                    fromEndToStart = true
+                )
+                if (startIndex1 == -1) {
+                    startIndex1 = 0
+                }
+                val endIndex1 = opIndex
+
+                println("startIndex2: ${startIndex1}")
+                println("endIndex2: ${endIndex1}")
+                var expression = result.substring(startIndex1 + 1, endIndex1) + op
+
+                val startIndex2 = opIndex + 1
+
+                println("result...: ${result}")
+                println("opIndex + 1: ${opIndex + 1}")
+                var endIndex2 = result.positionOf(
+                    {
+                        println(it)
+                        !digitsWithDot.contains(it)
+                    }, opIndex + 1
+                )
+                println("endIndex2: $endIndex2")
+                if (endIndex2 == -1) {
+                    println("q .")
+                    endIndex2 = result.length
+                }
+//                println("startIndex3: ${startIndex2}")
+//                println("endIndex3: ${endIndex2}")
+                expression += result.substring(startIndex2, endIndex2)
+
+                println("expression: ${expression}")
+                val calcResult = calcOperation(expression)
+                println("calcResult: ${calcResult}")
+                println("result a: ${result}")
+                result.replace(startIndex1 + 1, endIndex2, "${calcResult}")
+                println("result b: ${result}")
+//                println("temp: ${result.substring(startIndex1 + 1, endIndex2)}")
+
+            }
+        }
+
+
+        println("x0")
+        // 3. Вычисляем простые(бинарные) операции (+, -)
+        println("result c: ${result}")
+        println("x1")
+        val end1 = result.positionOf(")")
+        var start1 = result.positionOf(
+            "(",
+            startIndex = end1,
+            fromEndToStart = true
+        )
+        if (start1 == -1) {
+            start1 = 0
+        }
+        println("x2")
+        println("start1; $start1")
+        println("x3")
+//        val end1 = result.positionOf(")")
+        val blockExpression = result.substring(start1 + 1, end1)
+        println("blockExpression: $blockExpression")
+        val calcResult = calcOperation(blockExpression.toString())
+        println("calcOperation: $calcResult")
+        result.replace(start1 + 1, end1, calcResult.toString())
+        println("result: $result")
+        println("x4")
+        // 3. Убираем скобки
+        val end2 = result.positionOf(")")
+        var start2 = result.positionOf(
+            "(",
+            startIndex = end2,
+            fromEndToStart = true
+        )
+        if (start2 == -1) {
+            start2 = 0
+        }
+
+        println("x5")
+        result.replace(start2, start2 + 1, "")
+        println("x6")
+        val end3 = result.positionOf(")")
+        println("subs x: ${result.substring(end3, end3 + 1)}")
+        result.replace(end3, end3 + 1, "")
+        println("result x: $result")
+
+        // 2. Затем переносим операцию за скобки
+//        2*(-1) -> -2*(1) встречая * двигается дальше
+//        2/(-1) -> -2/(1) встречая / двигается дальше
+//        2%(-1) -> -2%(1) встречая % двигается дальше
+
+//        2+(-1) -> 2-(1) встречая + остается собой
+//        2-(-1) -> 2+(1) встречая - меняется на противоположное
+
+        // NOTE: по сути + и - это true и false
+        // и следовательно подчиняются булевой логике при переносе знака
+        // и остаются собой при арифметических операциях-контейнерах
+        // (которые раскладывают число по контейнерам(умножают, делят и т.д.))
+
+        if (ops.contains(calcResult.toString()[0])) { // если + или - то переносим знак
+            val op = calcResult.toString()[0]
+            val notOp = ops.filter { it != op }
+            while (true) {
+                val index = result.positionOf("$op", start2)
+                println("op: $op")
+                println("start: $start2")
+                println("index: $index")
+                if (index == -1) {
+                    break
+                }
+
+                // переносим знак до тех пор пока не встретим другой boolean-знак(+/-)
+                result.replace(index, index + 1, "") // удаляем знак
+//                println("result2: ${result}")
+                for (i in index downTo 0) {
+                    val c = result[i]
+
+                    when (c) {
+                        '+' -> {
+//                            println("c: ${c} | ${result.substring(i, i + 1)} | $op")
+                            result.replace(i, i + 1, "$op") // оставляем знак
+                            println("result3: ${result}")
+                            break
+                        }
+
+                        '-' -> result.replace(i, i + 1, "$notOp") // меняем знак
+                    }
+                    if (i == 0 && !ops.contains(c)) {
+                        result.insert(0, op)
+                    }
+                }
+            }
+        }
+    }
+
+    return result.toString()
 }
 
 // todo: обработать умножение на 2*(-1)
 // in: ((2*(-1)))
 // out: (((2*)-1.0))
-// expected: -2.0
+// expected: ((2*(-1)))
 private fun addBrackets(data: String): String {
     val source = if (data.first() != '(') {
         StringBuilder("($data)")
@@ -50,7 +298,7 @@ private fun addBrackets(data: String): String {
                 } else if (source[leftIndex - 1] == '(') {
                     bracketsCount -= 1
                 }
-                leftIndex = leftIndex - 1
+                leftIndex -= 1
             }
 
             bracketsCount = 0
@@ -70,7 +318,7 @@ private fun addBrackets(data: String): String {
                 } else if (source[rightIndex + 1] == ')') {
                     bracketsCount -= 1
                 }
-                rightIndex = rightIndex + 1
+                rightIndex += 1
             }
             i = rightIndex + 1
         }
@@ -82,7 +330,7 @@ private fun addBrackets(data: String): String {
     return source.toString()
 }
 
-fun calcMath(rowExpression: String): String {
+fun calcMath2(rowExpression: String): String {
     if (rowExpression.isEmpty()) {
         return rowExpression
     }
@@ -98,7 +346,7 @@ fun calcMath(rowExpression: String): String {
     val result = withBrackets.replaceRange(
         startIndex - 1,
         endIndex + 1,
-        "${calc(nextExpressionBlock)}"
+        "${calcOperation(nextExpressionBlock)}"
     )
     println("result: $result")
     return if (result.indexOf('(') == -1) {
@@ -108,7 +356,7 @@ fun calcMath(rowExpression: String): String {
     }
 }
 
-private fun calc(expression: String): Double {
+private fun calcOperation(expression: String): Double {
     val atoms = mutableListOf<String>()
 
     var number = ""
@@ -124,13 +372,13 @@ private fun calc(expression: String): Double {
                 it == '*' || it == '/' || it == '+' || it == '-' -> {
                     atoms.add(number.trim())
                     atoms.add("$it")
-                    println("add: $it")
+//                    println("add: $it")
                     number = ""
                 }
 
                 else -> {
-                    number = "$number$it"
-                    println("number: $number")
+                    number += it
+//                    println("number: $number")
                 }
             }
         }
